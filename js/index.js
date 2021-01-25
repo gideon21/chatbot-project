7b97dd507da85d2c4ab45b80c6e99355dcca5653
gideon shobowale <goshobowale@uclan.ac.uk>
2021-01-25 23:50:28 +0000

    new commit

--- js/index.js
+++ js/index.js
@@ -69,23 +69,23 @@ document.addEventListener("DOMContentLoaded", () => {
   function addChat(input, product) {
     const messagesContainer = document.getElementById("messages");
 
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.className = "user response";
+    userDiv.innerHTML = `<img src="images/user.png" class="avatar"><span>${input}</span>`;
+    messagesContainer.appendChild(userDiv);
+
     let botDiv = document.createElement("div");
     let botImg = document.createElement("img");
     let botText = document.createElement("span");
     botDiv.id = "bot";
-    botImg.src = "images/bot-mini.png";
+    botImg.src = "images/geo.png";
     botImg.className = "avatar";
     botDiv.className = "bot response";
     botText.innerText = "Typing...";
     botDiv.appendChild(botText);
     botDiv.appendChild(botImg);
     messagesContainer.appendChild(botDiv);
-  
-    let userDiv = document.createElement("div");
-    userDiv.id = "user";
-    userDiv.className = "user response";
-    userDiv.innerHTML = `<img src="images/user.png" class="avatar"><span>${input}</span>`;
-    messagesContainer.appendChild(userDiv);
   
   
     // Keep messages at most recent
